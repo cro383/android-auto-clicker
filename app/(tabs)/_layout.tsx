d6700fc863fe37e47d91e51c00a745086f6abb58
cro383 <cro9301@gmail.com>
2026-07-20 15:49:40 +0900

    Fix blank home screen after tab removal

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,5 @@
-import { Stack } from 'expo-router';
+import { Slot } from 'expo-router';
 
 export default function TabLayout() {
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-    </Stack>
-  );
+  return <Slot />;
 }
